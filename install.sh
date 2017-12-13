76ef0956ec71f2a9c4e2a44edc3b633d0e508298
Hitesh <hiteshjoshi1@yahoo.co.in>
2017-12-14 02:42:47 +0530

    docker with config end to end complete

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo docker rm eureka
-sudo docker rm config
-sudo docker rm customer1
-
+#sudo docker rm eureka
+#sudo docker rm config
+#sudo docker rm customer1
 
 cd  servicediscovery
 echo 'Build discovery'
@@ -18,12 +17,22 @@ echo 'Build customer'
 mvn clean package -DskipTests
 
 
+cd  ../inventory
+echo 'Build inventory'
+mvn clean package -DskipTests
+
+cd  ../invoice
+echo 'Build invoice'
+mvn clean package -DskipTests
+
 
 cd ../
 echo 'Copying .....'
 cp servicediscovery/target/servicediscovery-0.0.1-SNAPSHOT.jar /home/hitesh/jarloc
 cp config/target/config-0.0.1-SNAPSHOT.jar /home/hitesh/jarloc
 cp customer/target/customer-0.0.1-SNAPSHOT.jar /home/hitesh/jarloc
+cp inventory/target/inventory-0.0.1-SNAPSHOT.jar /home/hitesh/jarloc
+cp invoice/target/invoice-0.0.1-SNAPSHOT.jar /home/hitesh/jarloc
 
 
 sudo docker-compose up
