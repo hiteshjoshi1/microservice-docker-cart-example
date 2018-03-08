26a991498e2acd320ee065db29f4a01428d4b66c
Hitesh Joshi <hiteshjoshi1@yahoo.co.in>
2018-03-08 15:42:12 +0530

    Modifying docker and init config

--- install.sh
+++ install.sh
@@ -1,54 +1,24 @@
 #!/bin/bash
 echo 'Creating Volume Mount for Docker...'
-mkdir -p /home/${USER}/jarloc
+mkdir -p ${HOME}/jarloc
+
+echo 'getting config from git...'
+if [[ ! -d "${HOME}/jarloc/microservice-docker-cart-config" ]]; then
+git clone git@github.com:hiteshjoshi1/microservice-docker-cart-config.git ${HOME}/jarloc/microservice-docker-cart-config
+fi
 
 echo 'Building the entire project ...'
 mvn clean package -DskipTests
 
-# cd  servicediscovery
-# echo 'Build discovery...'
-# mvn clean package -DskipTests
-
-# cd ../config
-# echo 'Build config...'
-# mvn clean package -DskipTests
-
-# cd  ../customer
-
-# echo 'Build customer...'
-# mvn clean package -DskipTests
-
-
-# cd  ../inventory
-# echo 'Build inventory...'
-# mvn clean package -DskipTests
-
-# cd  ../invoice
-# echo 'Build invoice...'
-# mvn clean package -DskipTests
-
-
-# cd  ../monitor
-# echo 'Build monitor...'
-# mvn clean package -DskipTests
-
-# cd  ../zuulEdgeGateway
-# echo 'Build edge gateway...'
-# mvn clean package -DskipTests
-
-# cd  ../zipkinTimer
-# echo 'Build zipkin ui...'
-# mvn clean package -DskipTests
-# cd ../
 
 echo 'Copying .....'
-cp servicediscovery/target/servicediscovery-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp config/target/config-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp customer/target/customer-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp inventory/target/inventory-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp invoice/target/invoice-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp monitor/target/monitor-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp zuulEdgeGateway/target/zuulEdgeGateway-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
-cp zipkinTimer/target/zipkinTimer-0.0.1-SNAPSHOT.jar /home/${USER}/jarloc
+cp servicediscovery/target/servicediscovery-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp config/target/config-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp customer/target/customer-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp inventory/target/inventory-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp invoice/target/invoice-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp monitor/target/monitor-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp zuulEdgeGateway/target/zuulEdgeGateway-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
+cp zipkinTimer/target/zipkinTimer-0.0.1-SNAPSHOT.jar ${HOME}/jarloc
 echo 'All engines up, ready to launch to hyperspace .....'
 docker-compose up
