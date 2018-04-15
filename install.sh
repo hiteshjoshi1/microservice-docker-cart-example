b6d9bec9d865d9debff572dd32207e0c682581f3
Hitesh Joshi <hiteshjoshi1@yahoo.co.in>
2018-04-16 02:17:33 +0530

    ran entire setup in mac fixed git config

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 echo 'Creating Volume Mount for Docker...'
 mkdir -p ${HOME}/jarloc
 
-echo 'getting config from git...'
-if [[ ! -d "${HOME}/jarloc/microservice-docker-cart-config" ]]; then
-git clone git@github.com:hiteshjoshi1/microservice-docker-cart-config.git ${HOME}/jarloc/microservice-docker-cart-config
-fi
+#echo 'getting config from git...'
+#if [[ ! -d "${HOME}/jarloc/microservice-docker-cart-config" ]]; then
+#git clone git@github.com:hiteshjoshi1/microservice-docker-cart-config.git ${HOME}/jarloc/#microservice-docker-cart-config
+#fi
 
 echo 'Building the entire project ...'
 mvn clean package -DskipTests
